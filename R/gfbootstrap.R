ed7a4e52323396cd7040607c7d429bc906bc067f
Phil Dyer <phildyer@protonmail.com>
2020-04-23 10:58:40 +1000

    Fix missing variable name

--- R/gfbootstrap.R
+++ R/gfbootstrap.R
@@ -783,9 +783,9 @@ combinedBootstrapGF <- function(...,
     stop("Every argument must be a gradientForest")
 
   if(is.null(gf_names <- names(gf_list)))
-    gf_names <- paste("F",1:ngear,sep="")
+    gf_names <- paste("F",1:n_gf,sep="")
   if (any(empty <- gf_names==""))
-    gf_names[empty] <- paste("F",1:ngear,sep="")[empty]
+    gf_names[empty] <- paste("F",1:n_gf,sep="")[empty]
 
   names(gf_list) <- gf_names
 
